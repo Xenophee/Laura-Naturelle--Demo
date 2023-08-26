710bd7b0a3d973f345ba68008d29b181c5e8cfc1
Xenophee <perrine.morsch@gmail.com>
2023-08-26 17:19:18 +0200

    mise en place d'une condition d'affichage selon le support

--- public/assets/js/script.js
+++ public/assets/js/script.js
@@ -1,16 +1,24 @@
-const dropdown = document.querySelector('.drop-menu');
-const links = dropdown.querySelector('.drop-links');
 
-let timeoutId;
 
-dropdown.addEventListener('mouseenter', () => {
-    clearTimeout(timeoutId); // Annuler tout délai en cours
-    links.classList.remove('d-none');
-});
+// Affichage de la liste des prestations dans la barre de navigation pour les grands écrans
 
-dropdown.addEventListener('mouseleave', () => {
-    // Cacher le contenu après un délai de 350 millisecondes
-    timeoutId = setTimeout(() => {
-        links.classList.add('d-none');
-    }, 350);
-});
+if (window.innerWidth >= 1200) {
+
+    const dropdown = document.querySelector('.drop-menu');
+    const links = dropdown.querySelector('.drop-links');
+
+    let timeoutId;
+
+    dropdown.addEventListener('mouseenter', () => {
+        clearTimeout(timeoutId); // Annuler tout délai en cours
+        links.classList.remove('d-none');
+    });
+
+    dropdown.addEventListener('mouseleave', () => {
+        // Cacher le contenu après un délai de 350 millisecondes
+        timeoutId = setTimeout(() => {
+            links.classList.add('d-none');
+        }, 350);
+    });
+
+};
