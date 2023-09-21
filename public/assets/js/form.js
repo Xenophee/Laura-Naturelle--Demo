8e87b91b326c2514f66d3bbafa8d61a14599e2a3
Xenophee <perrine.dassonville@outlook.fr>
2023-09-21 19:50:17 +0200

    amélioration du dashboard et finalisation de la page promotion

--- public/assets/js/form.js
+++ public/assets/js/form.js
@@ -100,6 +100,27 @@ if (announcement != undefined) {
 };
 
 
+// ==============================================================================================
+// ----------------------------------------------------------------------------------------------
+// FORMULAIRE DE PROMOTION
+
+const discount = document.getElementById('discount-form');
+
+if (discount != undefined) {
+
+    const servicesBtn = document.querySelectorAll('input[name="whichServices"]');
+    
+    servicesBtn.forEach((element, key) => {
+        element.addEventListener('click', () => {
+            if (key == 1) {
+                servicesAccordion.classList.remove('d-none');
+            } else {
+                servicesAccordion.classList.add('d-none');
+            }
+        })
+    });
+}
+
 
 // ==============================================================================================
 // ----------------------------------------------------------------------------------------------
