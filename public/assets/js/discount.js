f68eff373a94d087ef6191fbae1a8b153051b4cc
Xenophee <perrine.dassonville@outlook.fr>
2023-10-31 17:40:13 +0100

    correctif mineur

--- public/assets/js/discount.js
+++ public/assets/js/discount.js
@@ -9,12 +9,28 @@ const discount = document.getElementById('discount-form');
 const servicesBtns = document.querySelectorAll('input[name="whichServices"]');
 
 
-for (const servicesBtn of servicesBtns) {
-    servicesBtn.addEventListener('input', () => {
-        if (servicesBtn.value == 2) {
+
+// ==============================================================================================
+// ----------------------------------------------------------------------------------------------
+// Affiche les accordéons de prestations en cas de choix ciblés
+
+const displayServices = () => {
+
+    for (const servicesBtn of servicesBtns) {
+
+        if (servicesBtn.checked && servicesBtn.value == 2) {
             servicesAccordion.classList.remove('d-none');
         } else {
             servicesAccordion.classList.add('d-none');
         };
-    });
+    };
 };
+
+
+displayServices();
+
+
+for (const servicesBtn of servicesBtns) {
+    servicesBtn.addEventListener('click', displayServices);
+};
+// ----------------------------------------------------------------------
